1fae85f50e0ea6ecaf3aa7a4d9914971761417ee
tj11 <tedwardj11@gmail.com>
2018-02-25 21:57:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ function sayHiToGrandma(string) {
     return `YES, INDEED!`
   }
   
-  if(string==="I love you, Grandma") {
-    return `I love you, too`
+  if(string==="I love you, Grandma.") {
+    return `I love you, too.`
   }
 }
 
